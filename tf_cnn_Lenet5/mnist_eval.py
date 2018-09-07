c69b34571d8611a70f766b20e09490788954ebc2
BigBigRadish <luozhukun@163.com>
2018-09-07 19:55:13 +0800

    cnn_lenet5

--- tf_cnn_Lenet5/mnist_eval.py
+++ tf_cnn_Lenet5/mnist_eval.py
@@ -10,16 +10,20 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import mnist_inference
 import mnist_train
+import numpy as np
 #每十秒加载一次最新的模型，并在测试数据上测试最新模型的正确率
 
 EVAL_INTERVAL_SECS = 10
 def evaluate(mnist):
     with tf.Graph().as_default() as g:
+        test_size=mnist.validation.images.size
         x = tf.placeholder(tf.float32, [None, mnist_inference.IMAGE_SIZE,mnist_inference.IMAGE_SIZE,mnist_inference.NUM_CHANNELS], name='x-input')
         y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-input')
-        validate_feed = {x : mnist.validation.images, y_:mnist.validation.labels}
+        print(np.array(mnist.validation.images))
+        xs=np.reshape(np.array(mnist.validation.images),(mnist.validation.images.size, mnist_inference.IMAGE_SIZE,mnist_inference.IMAGE_SIZE,mnist_inference.NUM_CHANNELS))
+        validate_feed = {x : xs, y_:mnist.validation.labels}
         #直接通过调用封装好的函数来计算前向传播的结果。因为测试时不关注正则化损失的值，所以这里用于计算正则化损失的函数被设置为None
-        y = mnist_inference.inference(x, None,None)
+        y = mnist_inference.inference(x, False,None)
         correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         variable_averages = tf.train.ExponentialMovingAverage(mnist_train.MOVING_AVERAGE_DECAY)
@@ -36,7 +40,7 @@ def evaluate(mnist):
                 else:
                     print('No checkpoint file found')
                     return
-                time.sleep(EVAL_INTERVAL_SECS)
+                #time.sleep(EVAL_INTERVAL_SECS)
 def main(argv=None):
     mnist = input_data.read_data_sets('./mnist/', one_hot=True)
     evaluate(mnist)
