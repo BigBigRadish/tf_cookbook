1191972f05a51479b405b08f33760c43f3d45244
BigBigRadish <luozhukun@163.com>
2018-09-09 17:05:48 +0800

    数据集的的高层操作api

---
+++ tf_data_procee_fram/tf_data_op_api.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2018年9月9日
+
+@author: Zhukun Luo
+Jiangxi university of finance and economics
+'''
+import tensorflow as tf
+#dataset=dataset.map(parse)#map方法对tfrcord进行解析操作
+#distorted_image=preprocess_for_train(decoded_image,image_size,image_size,None)
+#dataset=dataset.map(lambda x:preprocess_for_train(x,image_size,image_size,None))
+#dataset=dataset.shuffle(batch_size)#将数据随机打乱
+#dataset=dataset.batch(batch_size)#
+#dataset=dataset.repeat(N)#将一个数据集复制多份，repeat代表重复相同的处理过程，并不会记录前一步的处理结果
