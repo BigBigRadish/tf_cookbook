a2aa9f4a2b7d4c65d14b8062c863139667f46399
BigBigRadish <luozhukun@163.com>
2018-09-12 09:37:00 +0800

    PTB_vocab

--- tf_Nlp/tf_words_no2.py
+++ tf_Nlp/tf_words_no2.py
@@ -12,19 +12,25 @@ RAW_DATA='dataset/data/ptb.train.txt'#训练集数据文件
 VOCAB='dataset/data/ptb.vocab'#输出的词汇表文件
 OUTPUT_DATA='dataset/data/ptb.train'#将单词替换成单词编号后的输出
 
-with codecs.open(VOCAB, 'r', encoding='utf-8') as f_vocab:
-    vocab=[w.strip() for w in f_vocab.readline()]
-word_to_id={k:v for (k,v) in zip(vocab,range(len(vocab)))}
-#如果出现了被删除的低频词，则替换为‘《unk》’
+ 
+# 读取词汇表，并建立词汇到单词编号的映射。
+with codecs.open(VOCAB, "r", "utf-8") as f_vocab:
+    vocab = [w.strip() for w in f_vocab.readlines()]
+word_to_id = {k: v for (k, v) in zip(vocab, range(len(vocab)))}
+ 
+# 如果出现了不在词汇表内的低频词，则替换为"unk"。
+ 
+ 
 def get_id(word):
     return word_to_id[word] if word in word_to_id else word_to_id["<unk>"]
-
-fin=codecs.open(RAW_DATA,'r','utf-8')
-fout=codecs.open(OUTPUT_DATA, 'w', encoding='utf-8')
+ 
+ 
+fin = codecs.open(RAW_DATA, "r", "utf-8")
+fout = codecs.open(OUTPUT_DATA, 'w', 'utf-8')
 for line in fin:
-    words=line.strip().split()+['<eos>']#读取单词并添加《eos》结束符
-    #将每个单词替换为词汇表中的编号
-    out_line=' '.join([str(get_id(w)) for w in words]) +'\n'
+    words = line.strip().split() + ["<eos>"]  # 读取单词并添加<eos>结束符
+    # 将每个单词替换为词汇表中的编号
+    out_line = ' '.join([str(get_id(w)) for w in words]) + '\n'
     fout.write(out_line)
 fin.close()
 fout.close()
