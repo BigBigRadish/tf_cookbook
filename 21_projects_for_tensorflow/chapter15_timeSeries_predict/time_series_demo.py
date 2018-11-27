1293ee504329733cc41eac2e15a5fae2401b93c3
BigBigRadish <luozhukun@163.com>
2018-11-27 16:46:19 +0800

    tensorflow读取数据的两种方式

--- 21_projects_for_tensorflow/chapter15_timeSeries_predict/time_series_demo.py
+++ 21_projects_for_tensorflow/chapter15_timeSeries_predict/time_series_demo.py
@@ -5,13 +5,14 @@ Created on 2018年11月26日
 @author: Zhukun Luo
 Jiangxi university of finance and economics
 '''
+#tensorflow读取数据的两种方式
 from __future__ import print_function
 import numpy as np
 import matplotlib 
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.contrib.timeseries.python.timeseries import NumpyReader
+from tensorflow.contrib.timeseries.python.timeseries import NumpyReader,CSVReader
 #接着，利用np.sin生成一个实验用的时间序列数据，改时间序列数据实际上是在正弦曲线上加入了上升的趋势和一些随机的噪声
 x=np.array(range(1000))
 noise=np.random.uniform(-0.2,0.2,1000)
@@ -40,3 +41,14 @@ with tf.Session() as sess:
     one_batch=sess.run(batch_data[0])
     coord.request_stop()
 print(one_batch)
+#以csv的格式读入时间序列数据
+csv_file_name='./trend.csv'
+reader=CSVReader(csv_file_name)
+with tf.Session() as sess:
+    full_data=reader.read_full()#返回时间序列对应的tensor
+    #调用read_full()方法会生成读取队列
+    #调用tf.trian.start_queue_runners启动队列才能正常读取
+    coord=tf.train.Coordinator()
+    threads=tf.train.start_queue_runners(sess=sess, coord=coord)
+    print(sess.run(full_data))
+    coord.request_stop()
